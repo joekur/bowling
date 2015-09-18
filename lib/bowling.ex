9d46eb2afbf431f4e6d8a4dda1a35248d3394746
Joe Kurleto <joedoku@gmail.com>
2015-09-18 15:30:08 -0500

    Handle strike on 9th frame correctly

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,25 +1,21 @@
 defmodule Bowling do
   def score(rolls) do
-    score(rolls, 0)
+    score(rolls, 0, 1)
   end
 
-  defp score([a,b,c], total) when a == 10 or a + b == 10 do
-    total + a + b + c
+  defp score(rolls, total, 10) do
+    total + Enum.sum(rolls)
   end
 
-  defp score([], total) do
-    total
+  defp score([10,a,b|rest], total, frame) do
+    score([a,b|rest], total + 10 + a + b, frame + 1)
   end
 
-  defp score([10,a,b|rest], total) do
-    score([a,b|rest], total + 10 + a + b)
+  defp score([a,b,c|rest], total, frame) when a + b == 10 do
+    score([c|rest], total + 10 + c, frame + 1)
   end
 
-  defp score([a,b,c|rest], total) when a + b == 10 do
-    score([c|rest], total + 10 + c)
-  end
-
-  defp score([a,b|rest], total) do
-    score(rest, total + a + b)
+  defp score([a,b|rest], total, frame) do
+    score(rest, total + a + b, frame + 1)
   end
 end
